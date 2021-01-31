e4b6b1425aff21c1c0ebf6972581606b769efd28
Prasun Mondal <prasun.mondal@oracle.com>
2021-01-31 20:14:26 +0530

    create primary UIs

--- app/src/main/java/com/prasunmondal/tech4docs/MainActivity.kt
+++ app/src/main/java/com/prasunmondal/tech4docs/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.prasunmondal.tech4docs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToMoneyMinded(view: View) {
+        val myIntent = Intent(this, MoneyMinded::class.java)
+        this.startActivity(myIntent)
     }
 }
