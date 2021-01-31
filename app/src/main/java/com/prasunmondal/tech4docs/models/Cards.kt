b985c172a95be3b507a223d8330984ad5535c0ac
Prasun Mondal <prasun.mondal@oracle.com>
2021-01-31 21:13:33 +0530

    Add Card UI

--- app/src/main/java/com/prasunmondal/tech4docs/models/Cards.kt
+++ app/src/main/java/com/prasunmondal/tech4docs/models/Cards.kt
@@ -4,28 +4,31 @@ import com.prasunmondal.tech4docs.enums.CardType
 import com.prasunmondal.tech4docs.enums.CardsProvider
 
 class Cards {
-    var name: String
-    var number: Int
+    var cardType: CardType
+    var cardsProvider: CardsProvider
+    var cardNumber: Int
+    var cardHolderName: String
     var expiryDate: Int
     var expiryMonth: Int
     var CVV: Int
     var pin: Int
-    var cardsProvider: CardsProvider
-    var cardType: CardType
     var linkedBank: String
+    var name: String
+
 
     constructor(
-            number: Int,
-            expiryDate: Int,
-            expiryMonth: Int,
-            CVV: Int,
-            pin: Int,
-            name: String,
-            cardType: CardType,
-            cardsProvider: CardsProvider,
-            linkedBank: String
+        number: Int,
+        expiryDate: Int,
+        expiryMonth: Int,
+        CVV: Int,
+        pin: Int,
+        name: String,
+        cardType: CardType,
+        cardsProvider: CardsProvider,
+        linkedBank: String,
+        cardHolderName: String
     ) {
-        this.number = number
+        this.cardNumber = number
         this.expiryDate = expiryDate
         this.expiryMonth = expiryMonth
         this.CVV = CVV
@@ -34,5 +37,6 @@ class Cards {
         this.cardType = cardType
         this.cardsProvider = cardsProvider
         this.linkedBank = linkedBank
+        this.cardHolderName = cardHolderName
     }
 }
