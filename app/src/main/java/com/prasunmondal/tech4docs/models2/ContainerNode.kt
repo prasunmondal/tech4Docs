e4398848553c67757bf6cb2f30252a359d05c3a2
Prasun Mondal <prasun.mondal@oracle.com>
2021-04-11 10:49:31 +0530

    also add parent container node

--- app/src/main/java/com/prasunmondal/tech4docs/models2/ContainerNode.kt
+++ app/src/main/java/com/prasunmondal/tech4docs/models2/ContainerNode.kt
@@ -3,12 +3,14 @@ package com.prasunmondal.tech4docs.models2
 class ContainerNode {
     lateinit var name: String
     lateinit var containerNodes: ArrayList<ContainerNode>
+    lateinit var parentContainerNode: ContainerNode
     lateinit var dataNodes:  ArrayList<DataNode>
 
-    constructor(name: String) {
+    constructor(name: String, parentContainerNode: ContainerNode) {
         this.name = name
         containerNodes = arrayListOf()
         dataNodes = arrayListOf()
+        this.parentContainerNode = parentContainerNode
     }
 
     fun createDataNode(name: String) {
